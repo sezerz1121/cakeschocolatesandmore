d9305a21f9a73d4a71494c108ac208f21c9f1ac5
Tatsamva <145141011+Tatsamva@users.noreply.github.com>
2026-07-12 00:58:53 +0530

    Preserve mobile story link and reset scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/layout/Header.jsx";
 import Footer from "./components/layout/Footer.jsx";
 import Home from "./pages/Home.jsx";
@@ -7,11 +8,22 @@ import Desserts from "./pages/Desserts.jsx";
 import VisitUs from "./pages/VisitUs.jsx";
 import OurStory from "./pages/OurStory.jsx";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cakes" element={<Cakes />} />
